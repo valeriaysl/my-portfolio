23bf8f2e660c79910765575bd62d27be0e604fc2
Valeria Vega Herrera <carmen.vega@unadeca.net>
2025-05-22 00:22:27 -0600

    ✅ Setup Tailwind and confirmed base app is working

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-indigo-600 text-white h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Tailwind is ALIVE! 🎉</h1>
+    </div>
+  );
+}
+
+export default App;
